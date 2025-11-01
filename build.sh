2908e3d42e89a96f942e18e87a52c2018264211d
Filip Spasovski <f.spasovski96@gmail.com>
2025-11-01 02:10:10 +0100

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -11,15 +11,30 @@ docker build --platform linux/arm64 -t $IMAGE_NAME .
 
 echo "Extracting binary from Docker image..."
 CONTAINER_ID=$(docker create --platform linux/arm64 $IMAGE_NAME)
-docker cp $CONTAINER_ID:/app/pocketstream-app ./$OUTPUT_BINARY
+
+if [ -d "./Pocketstream" ]; then
+  rm -rf "./Pocketstream"
+fi
+
+mkdir ./Pocketstream
+
+docker cp $CONTAINER_ID:/app/pocketstream-app ./Pocketstream/$OUTPUT_BINARY
 docker rm $CONTAINER_ID
+cp ./font.ttf ./Pocketstream/font.ttf
+
+# Create muOS launcher script
+cat > "./Pocketstream/mux_launch.sh" << 'EOF'
+#!/bin/sh
+
+cd /mnt/mmc/MUOS/application/Pocketstream
+./pocketstream-app-arm64
+EOF
+
+chmod +x "./Pocketstream/mux_launch.sh"
 
 echo "Build complete!"
-echo "Binary: ./$OUTPUT_BINARY"
+echo "Binary: ./Pocketstream/$OUTPUT_BINARY"
 echo "Binary info:"
-file $OUTPUT_BINARY
-ls -lh $OUTPUT_BINARY
+file ./Pocketstream/$OUTPUT_BINARY
+ls -lh ./Pocketstream/$OUTPUT_BINARY
 
-echo "To verify the binary, you can run:"
-echo "   ldd $OUTPUT_BINARY  # (on Linux system) to check dependencies"
-echo "Transfer to your device and run!"
